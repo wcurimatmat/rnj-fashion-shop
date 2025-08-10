33000f1698221a1809e4d79887d4110732a1c00f
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-10 12:19:22 +0800

    fix(NavItems): update navigation item from "About" to "Our Story" and include "Contact Us"

--- resources/js/User/Utils/NavItems.js
+++ resources/js/User/Utils/NavItems.js
@@ -1,7 +1,8 @@
 const navItems = [
     { name: "Home", url: "#" },
     { name: "Shop", url: "#" },
-    { name: "About", url: "#" },
+    { name: "Our Story", url: "#" },
+    { name: "Contact Us", url: "#" },
 ];
 
 export default navItems;
