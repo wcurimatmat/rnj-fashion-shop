8161e778bf8adf85dbcaa2c1320d77c41d6b3dec
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-10 10:16:04 +0800

    style(Header): app logo style
    - remove 'aspect-ratio' class
    - add 'flex-1' for breakpoint 'md'

--- resources/js/User/Components/Header/AppLogo.jsx
+++ resources/js/User/Components/Header/AppLogo.jsx
@@ -2,7 +2,7 @@ import Logo from "@Assets/logo/logo.png";
 
 function AppLogo() {
     return (
-        <figure className="aspect-square overflow-hidden">
+        <figure className="overflow-hidden md:flex-1">
             <a href="">
                 <img src={Logo} alt="" className="w-16 object-cover" />
             </a>
