165af44bab821db7136554d5b2f53136ae09ebfa
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-10 12:17:22 +0800

    style(Nav): update background color

--- resources/js/User/Components/Header/Nav.jsx
+++ resources/js/User/Components/Header/Nav.jsx
@@ -5,7 +5,7 @@ function Nav({ navState }) {
         <nav
             className={`fixed top-20 left-4 md:static ${navState ? "translate-none" : "-translate-x-[120%]"} z-2 transition duration-150 ease-in-out md:translate-none`}
         >
-            <div className="rounded-sm bg-rose-300 px-12 md:bg-transparent">
+            <div className="rounded-sm bg-rose-200 px-12 md:bg-transparent">
                 <ul className="flex flex-col divide-y md:flex-row md:gap-8 md:divide-none">
                     {NavItems.map(function (item, index) {
                         return (
