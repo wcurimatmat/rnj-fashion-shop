4f7a65ffe138d9aef2ee800a694a9b58adbc292e
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-10 08:12:05 +0800

    feat(Header): add Header component

--- resources/js/User/Components/Header/Header.jsx
+++ resources/js/User/Components/Header/Header.jsx
@@ -1,12 +1,31 @@
 import AppLogo from "./AppLogo";
 
-import 
+import { Menu, ShoppingBag, CircleUserRound } from "lucide-react";
 
 function Header() {
     return (
         <header>
-            <div className="">
+            <div className="flex align-center justify-between">
+                <button className="">
+                    <Menu size={26} />
+                </button>
+
                 <AppLogo />
+
+                <div className="">
+                    <ul className="flex align-center gap-4">
+                        <li>
+                            <a href="">
+                                <ShoppingBag size={26} />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <CircleUserRound size={26} />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </header>
     );
