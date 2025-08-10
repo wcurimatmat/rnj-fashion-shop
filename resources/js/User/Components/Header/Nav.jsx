a30ddb5a7cdfa06b5d277b26ec1f7fa10bb0e174
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-10 10:40:31 +0800

    feat(Header): add new features on Nav component
    - add 'navState' prop
    - add conditional styling for 'translate' style
    - add z-index to put the Nav component on top of overlay background
    - add transition effects

--- resources/js/User/Components/Header/Nav.jsx
+++ resources/js/User/Components/Header/Nav.jsx
@@ -1,8 +1,10 @@
 import NavItems from "@User/Utils/NavItems";
 
-function Nav() {
+function Nav({ navState }) {
     return (
-        <nav className="fixed top-20 left-4 md:static">
+        <nav
+            className={`fixed top-20 left-4 md:static ${navState ? "translate-none" : "-translate-x-[120%]"} z-2 transition duration-150 ease-in-out`}
+        >
             <div className="rounded-sm bg-rose-300 px-12">
                 <ul className="flex flex-col divide-y">
                     {NavItems.map(function (item, index) {
