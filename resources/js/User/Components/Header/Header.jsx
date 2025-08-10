04a68a01caa4dbe7e84a7b2cbe633b1e7e732c1a
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-10 09:10:55 +0800

    style(Header): login button element
    - change background color utility to [bg-rose-400]
    - add hover effect that change its background color to [bg-rose-300]
    - add transition effect

--- resources/js/User/Components/Header/Header.jsx
+++ resources/js/User/Components/Header/Header.jsx
@@ -15,7 +15,7 @@ function Header() {
                 <div className="flex flex-1 justify-end">
                     <a
                         href=""
-                        className="rounded-sm bg-rose-300 px-4 py-2 font-bold text-gray-100"
+                        className="rounded-sm bg-rose-400 px-4 py-2 font-bold text-gray-100 transition duration-200 ease-in-out hover:bg-rose-300"
                     >
                         Login
                     </a>
