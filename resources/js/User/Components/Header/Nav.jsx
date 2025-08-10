ea45a1b7e2f72e52f011924d60f73daa3ba15cc4
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-10 12:12:19 +0800

    feat(Nav): update Nav component styles for better responsiveness

--- resources/js/User/Components/Header/Nav.jsx
+++ resources/js/User/Components/Header/Nav.jsx
@@ -3,16 +3,16 @@ import NavItems from "@User/Utils/NavItems";
 function Nav({ navState }) {
     return (
         <nav
-            className={`fixed top-20 left-4 md:static ${navState ? "translate-none" : "-translate-x-[120%]"} z-2 transition duration-150 ease-in-out`}
+            className={`fixed top-20 left-4 md:static ${navState ? "translate-none" : "-translate-x-[120%]"} z-2 transition duration-150 ease-in-out md:translate-none`}
         >
-            <div className="rounded-sm bg-rose-300 px-12">
-                <ul className="flex flex-col divide-y">
+            <div className="rounded-sm bg-rose-300 px-12 md:bg-transparent">
+                <ul className="flex flex-col divide-y md:flex-row md:gap-8 md:divide-none">
                     {NavItems.map(function (item, index) {
                         return (
                             <li key={index} className="py-4">
                                 <a
                                     href={item.url}
-                                    className="font-quattrocento text-xl"
+                                    className="font-quattrocento text-xl md:text-base"
                                 >
                                     {item.name}
                                 </a>
