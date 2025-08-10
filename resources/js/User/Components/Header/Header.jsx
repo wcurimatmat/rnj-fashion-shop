332a429d2e40661aede357b22c2200d7fa20437b
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-10 10:48:28 +0800

    feat(Header): new features for Header component
    - add state handling
    - add state handler function
    - use state handler function for menu button click event
    - pass 'navState' to Nav component as prop
    - add overlay that conditionally rendering according to 'navState'

--- resources/js/User/Components/Header/Header.jsx
+++ resources/js/User/Components/Header/Header.jsx
@@ -1,27 +1,45 @@
 import AppLogo from "./AppLogo";
-
+import Nav from "./Nav";
 import { Menu } from "lucide-react";
+import { useState } from "react";
 
 function Header() {
+    const [navState, setNavState] = useState(false);
+
+    function handleSetNavState() {
+        setNavState(!navState);
+    }
+
     return (
-        <header>
-            <div className="flex items-center justify-between px-4 py-5">
-                <button className="flex-1 cursor-pointer">
-                    <Menu size={26} />
-                </button>
-
-                <AppLogo />
-
-                <div className="flex flex-1 justify-end">
-                    <a
-                        href=""
-                        className="rounded-sm bg-rose-400 px-4 py-2 font-bold text-gray-100 transition duration-200 ease-in-out hover:bg-rose-300"
+        <>
+            <header>
+                <div className="flex items-center justify-between px-4 py-5">
+                    <button
+                        className="flex-1 cursor-pointer md:hidden"
+                        onClick={handleSetNavState}
                     >
-                        Login
-                    </a>
+                        <Menu size={26} />
+                    </button>
+
+                    <AppLogo />
+
+                    <Nav navState={navState} />
+
+                    <div className="flex flex-1 justify-end">
+                        <a
+                            href=""
+                            className="rounded-sm bg-rose-400 px-4 py-2 font-bold text-gray-100 transition duration-200 ease-in-out hover:bg-rose-300"
+                        >
+                            Login
+                        </a>
+                    </div>
                 </div>
-            </div>
-        </header>
+            </header>
+            <div
+                className={`fixed top-0 left-0 z-1 h-screen w-screen bg-gray-200 ${navState ? "visible opacity-65" : "invisible opacity-0"} transition duration-200 ease-in-out`}
+                onClick={handleSetNavState}
+            ></div>
+        </>
     );
 }
 
