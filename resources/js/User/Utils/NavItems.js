9da45e54e9115e4ac0cfdb9e1cc0ecaacf6c5027
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-11 08:00:10 +0800

    feat(NavItems): restructure navItems and add new links

--- resources/js/User/Utils/NavItems.js
+++ resources/js/User/Utils/NavItems.js
@@ -1,8 +1,19 @@
-const navItems = [
-    { name: "Home", url: "#" },
-    { name: "Shop", url: "#" },
-    { name: "Our Story", url: "#" },
-    { name: "Contact Us", url: "#" },
-];
+const navItems = {
+    pages: [
+        { name: "Home", url: "#" },
+        { name: "Shop", url: "#" },
+        { name: "Our Story", url: "#" },
+        { name: "Contact Us", url: "#" },
+    ],
+    legals: [
+        { name: "Terms", url: "#" },
+        { name: "Privacy Policy", url: "#" },
+        { name: "Cookies Policy", url: "#" },
+    ],
+    help: [
+        { name: "FAQ", url: "#" },
+        { name: "Support", url: "#" },
+    ],
+};
 
 export default navItems;
