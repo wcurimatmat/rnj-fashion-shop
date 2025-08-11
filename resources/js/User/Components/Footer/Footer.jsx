abf957cdc4142ee067ba520394302a85e9ce9afc
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-11 08:59:30 +0800

    style(Footer): add responsive padding for medium screen sizes

--- resources/js/User/Components/Footer/Footer.jsx
+++ resources/js/User/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import ContactUs from "./ContactUs";
 
 function Footer() {
     return (
-        <footer className="bg-rose-200 px-4 py-8">
+        <footer className="bg-rose-200 px-4 py-8 md:px-16">
             <div className="grid gap-8">
                 <Shop />
                 <Nav />
