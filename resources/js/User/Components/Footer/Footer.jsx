45ab74f54f0cca38ebb6da6fa6b8a862306a50de
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-11 09:19:10 +0800

    style(Footer): enhance layout spacing and responsiveness for footer components

--- resources/js/User/Components/Footer/Footer.jsx
+++ resources/js/User/Components/Footer/Footer.jsx
@@ -4,10 +4,10 @@ import ContactUs from "./ContactUs";
 
 function Footer() {
     return (
-        <footer className="bg-rose-200 px-4 py-8 md:px-16">
-            <div className="grid gap-8 lg:max-w-[1440px] m-auto lg:flex lg:justify-between">
+        <footer className="bg-rose-200 px-4 py-16 md:px-16 lg:lg:px-24">
+            <div className="m-auto grid gap-8 lg:flex lg:max-w-[1440px] lg:justify-between">
                 <Shop />
-                <div className="grid gap-8 md:flex md:justify-between">
+                <div className="grid gap-8 md:flex md:justify-between lg:gap-20">
                     <Nav />
                     <ContactUs />
                 </div>
