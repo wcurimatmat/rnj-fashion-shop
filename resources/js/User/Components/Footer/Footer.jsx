9d5deda84c22152e6f82d48020795bb20d712bad
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-11 09:14:57 +0800

    style(Footer): add responsive design for breakpoint 'lg'

--- resources/js/User/Components/Footer/Footer.jsx
+++ resources/js/User/Components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import ContactUs from "./ContactUs";
 function Footer() {
     return (
         <footer className="bg-rose-200 px-4 py-8 md:px-16">
-            <div className="grid gap-8 lg:max-w-[1440px] m-auto">
+            <div className="grid gap-8 lg:max-w-[1440px] m-auto lg:flex lg:justify-between">
                 <Shop />
                 <div className="grid gap-8 md:flex md:justify-between">
                     <Nav />
