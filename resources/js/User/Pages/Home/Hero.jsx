46b9beaf4b70aa9903f2ebbd848b13ea32a7fdb2
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-12 11:29:40 +0800

    refactor(Hero): simplify Hero component structure and add styling

--- resources/js/User/Pages/Home/Hero.jsx
+++ resources/js/User/Pages/Home/Hero.jsx
@@ -1,17 +1,16 @@
-import HeroImage from "@Assets/visuals/pexels-migsr87-8051256.jpg";
-
 function Hero() {
     return (
-        <section>
-            <div className="">
-                <figure>
-                    <img src={HeroImage} alt="" />
-                </figure>
-
-                <div className="">
-                    <h1>Fashion That Fits You Perfectly </h1>
-                    <a href="#">Explore Collection</a>
-                </div>
+        <section className="hero-background rounded-lg py-36 font-bold text-gray-100 lg:py-56">
+            <div className="grid gap-8 text-center">
+                <h1 className="font-quattrocento text-2xl md:text-4xl lg:text-5xl">
+                    Fashion That Fits You Perfectly
+                </h1>
+                <a
+                    href="#"
+                    className="m-auto w-max rounded-sm border-2 border-rose-200 px-8 py-4"
+                >
+                    Explore Collection
+                </a>
             </div>
         </section>
     );
