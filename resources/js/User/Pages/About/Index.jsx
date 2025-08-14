f9a46436e1ba594612cc82ce4ccc7aa03b687a9f
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-14 14:13:37 +0800

    feat(routes): add route definition for About page

---
+++ resources/js/User/Pages/About/Index.jsx
@@ -0,0 +1,43 @@
+function About() {
+    return (
+        <>
+            <section>
+                <div className="">
+                    <h1>About Us</h1>
+                </div>
+
+                <div className="">
+                    <p>
+                        At R&J Fashion Shop, we believe fashion is more than
+                        just clothing—it’s a way to express confidence,
+                        creativity, and individuality. Our curated collections
+                        are designed with today’s woman in mind, offering styles
+                        that transition seamlessly from casual days to elegant
+                        evenings.
+                    </p>
+
+                    <p>
+                        From timeless wardrobe staples to the latest trends,
+                        each piece is carefully selected for quality, comfort,
+                        and a touch of sophistication. Whether you’re looking
+                        for a chic everyday outfit, a statement dress for a
+                        special occasion, or the perfect accessories to complete
+                        your look, we’ve got you covered.
+                    </p>
+
+                    <p>
+                        Our mission is simple: to help you look and feel your
+                        best, every day. With a passion for style and a
+                        commitment to exceptional service, R&J Fashion Shop is
+                        your go-to destination for fashion that inspires
+                        confidence and celebrates who you are.
+                    </p>
+                </div>
+            </section>
+            <section></section>
+            <section></section>
+        </>
+    );
+}
+
+export default About;
