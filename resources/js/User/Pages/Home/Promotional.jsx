7bd50031b9b2b70a70005f1bc1fbcc2f91d8071c
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-14 11:44:51 +0800

    feat(Promotional): create Promotional component

---
+++ resources/js/User/Pages/Home/Promotional.jsx
@@ -0,0 +1,12 @@
+function Promotional() {
+    return (
+        <section>
+            <div className="">
+                <h2>Chic, Comfortable, and Always on Trend</h2>
+                <a href="">Explore Collections</a>
+            </div>
+        </section>
+    );
+}
+
+export default Promotional;
