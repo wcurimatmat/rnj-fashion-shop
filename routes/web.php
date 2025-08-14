13343bc5e3990280ee06a0936174235f85fd408f
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-14 14:12:55 +0800

    feat(routes): add route definition for Shop page

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return Inertia::render('Home/Index');
 });
+
+Route::get('/shop', function () {
+    return Inertia::render("Shop/Index");
+});
