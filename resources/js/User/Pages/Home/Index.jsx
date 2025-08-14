edfe0e1031ee5a72db94a8d1fc450d7dbb426481
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-14 08:35:05 +0800

    feat(Proposition): create Proposition component

--- resources/js/User/Pages/Home/Index.jsx
+++ resources/js/User/Pages/Home/Index.jsx
@@ -2,6 +2,7 @@ import UserLayout from "@User/Layout/UserLayout";
 
 import Hero from "./Hero";
 import Features from "./Features";
+import Proposition from "./Proposition";
 
 function Index() {
     return (
@@ -9,6 +10,7 @@ function Index() {
             <div className="grid gap-8">
                 <Hero />
                 <Features />
+                <Proposition />
             </div>
         </UserLayout>
     );
