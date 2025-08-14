625d66b63c4566b56eb9a548f6764ab2ef09c84c
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-14 14:36:04 +0800

    feat(About): fix markup structure of About page and add image for visual

--- resources/js/User/Pages/About/Index.jsx
+++ resources/js/User/Pages/About/Index.jsx
@@ -1,48 +1,55 @@
 import UserLayout from "@User/Layout/UserLayout";
+import AboutUsVisual from "@Assets/visuals/pexels-liza-summer-6347546.jpg";
 
 function About() {
     return (
         <UserLayout>
             <section className="m-auto grid max-w-2xl gap-8">
                 <div className="">
-                    <h1 className="font-quattrocento text-2xl font-bold md:text-4xl lg:text-5xl">
-                        About Us
-                    </h1>
+                    <div className="">
+                        <h1 className="font-quattrocento text-2xl font-bold md:text-4xl lg:text-5xl">
+                            About Us
+                        </h1>
+                    </div>
+
+                    <div className="grid gap-6 leading-7">
+                        <p>
+                            At{" "}
+                            <span className="font-quattrocento font-bold text-rose-400">
+                                R&J Fashion Shop
+                            </span>
+                            , we believe fashion is more than just clothing—it’s
+                            a way to express confidence, creativity, and
+                            individuality. Our curated collections are designed
+                            with today’s woman in mind, offering styles that
+                            transition seamlessly from casual days to elegant
+                            evenings.
+                        </p>
+                        <p>
+                            From timeless wardrobe staples to the latest trends,
+                            each piece is carefully selected for quality,
+                            comfort, and a touch of sophistication. Whether
+                            you’re looking for a chic everyday outfit, a
+                            statement dress for a special occasion, or the
+                            perfect accessories to complete your look, we’ve got
+                            you covered.
+                        </p>
+                        <p>
+                            <span className="font-bold">
+                                Our mission is simple:
+                            </span>{" "}
+                            to help you look and feel your best, every day. With
+                            a passion for style and a commitment to exceptional
+                            service, R&J Fashion Shop is your go-to destination
+                            for fashion that inspires confidence and celebrates
+                            who you are.
+                        </p>
+                    </div>
                 </div>
 
-                <div className="grid gap-6 leading-7">
-                    <p>
-                        At{" "}
-                        <span className="font-quattrocento font-bold text-rose-400">
-                            R&J Fashion Shop
-                        </span>
-                        , we believe fashion is more than just clothing—it’s a
-                        way to express confidence, creativity, and
-                        individuality. Our curated collections are designed with
-                        today’s woman in mind, offering styles that transition
-                        seamlessly from casual days to elegant evenings.
-                    </p>
-
-                    <p>
-                        From timeless wardrobe staples to the latest trends,
-                        each piece is carefully selected for quality, comfort,
-                        and a touch of sophistication. Whether you’re looking
-                        for a chic everyday outfit, a statement dress for a
-                        special occasion, or the perfect accessories to complete
-                        your look, we’ve got you covered.
-                    </p>
-
-                    <p>
-                        <span className="font-bold">
-                            Our mission is simple:
-                        </span>{" "}
-                        to help you look and feel your best, every day. With a
-                        passion for style and a commitment to exceptional
-                        service, R&J Fashion Shop is your go-to destination for
-                        fashion that inspires confidence and celebrates who you
-                        are.
-                    </p>
-                </div>
+                <figure>
+                    <img src={AboutUsVisual} alt="" />
+                </figure>
             </section>
         </UserLayout>
     );
