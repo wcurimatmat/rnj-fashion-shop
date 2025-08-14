e5c10a6ea83173268fb2c17c243851f7a3e575ab
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-14 13:33:40 +0800

    style(Header): add styles for Header component

--- resources/js/User/Pages/Shop/Header.jsx
+++ resources/js/User/Pages/Shop/Header.jsx
@@ -1,7 +1,9 @@
 function Header() {
     return (
         <section>
-            <h1>All Items</h1>
+            <h1 className="font-quattrocento text-2xl font-bold md:text-3xl lg:text-4xl">
+                All Items
+            </h1>
         </section>
     );
 }
