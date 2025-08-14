6a6e9c9056ddd8f3a02d1b825939c1ece7abf063
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-14 13:37:46 +0800

    style(ItemCollection): add styles for ItemCollection component

--- resources/js/User/Pages/Shop/ItemCollections.jsx
+++ resources/js/User/Pages/Shop/ItemCollections.jsx
@@ -4,7 +4,25 @@ function ItemCollections() {
     return (
         <section>
             <div className="">
-                <ul className="">
+                <ul className="grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-6 lg:grid-cols-4 lg:gap-12">
+                    <li>
+                        <ProductCard />
+                    </li>
+                    <li>
+                        <ProductCard />
+                    </li>
+                    <li>
+                        <ProductCard />
+                    </li>
+                    <li>
+                        <ProductCard />
+                    </li>
+                    <li>
+                        <ProductCard />
+                    </li>
+                    <li>
+                        <ProductCard />
+                    </li>
                     <li>
                         <ProductCard />
                     </li>
