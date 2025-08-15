39e0d2ee9603f3640058aa461ee0b8dca19fb2d4
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-15 08:26:53 +0800

    feat(Login): create Login component

---
+++ resources/js/User/Pages/Auth/Login.jsx
@@ -0,0 +1,27 @@
+import UserLayout from "@User/Layout/UserLayout";
+
+function Login() {
+    return (
+        <UserLayout>
+            <section className="">
+                <h1>Login</h1>
+                <p>Enter your email and password to login:</p>
+            </section>
+
+            <form action="">
+                <div className="">
+                    <label htmlFor="">Email</label>
+                    <input type="text" name="" id="" />
+                </div>
+                <div className="">
+                    <label htmlFor="">Password</label>
+                    <input type="password" name="" id="" />
+                </div>
+
+                <button type="submit">Login</button>    
+            </form>
+        </UserLayout>
+    );
+}
+
+export default Login;
