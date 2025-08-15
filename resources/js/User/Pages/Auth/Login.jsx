1c5c079d8228c16edf6e85ae6632cb489c7af631
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-15 08:54:49 +0800

    style(Login): add styles for Login page

--- resources/js/User/Pages/Auth/Login.jsx
+++ resources/js/User/Pages/Auth/Login.jsx
@@ -3,25 +3,51 @@ import UserLayout from "@User/Layout/UserLayout";
 function Login() {
     return (
         <UserLayout>
-            <section className="">
-                <h1>Login</h1>
-                <p>Enter your email and password to login:</p>
-            </section>
+            <div className="m-auto grid max-w-md gap-8 py-20">
+                <section className="text-center">
+                    <h1 className="font-quattrocento mb-6 text-3xl font-bold">
+                        Login
+                    </h1>
+                    <p>Enter your email and password to login:</p>
+                </section>
 
-            <section>
-                <form action="">
-                    <div className="">
-                        <label htmlFor="">Email</label>
-                        <input type="text" name="" id="" />
-                    </div>
-                    <div className="">
-                        <label htmlFor="">Password</label>
-                        <input type="password" name="" id="" />
-                    </div>
+                <section>
+                    <form action="" className="grid gap-6">
+                        <div className="flex flex-col gap-2">
+                            <label htmlFor="">Email</label>
+                            <input
+                                type="text"
+                                name=""
+                                id=""
+                                placeholder="E-mail"
+                                className="rounded-md border border-gray-300 p-4 outline-transparent focus:border-rose-300"
+                            />
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <label htmlFor="">Password</label>
+                            <input
+                                type="password"
+                                name=""
+                                id=""
+                                placeholder="Password"
+                                className="rounded-md border border-gray-300 p-4 outline-transparent focus:border-rose-300"
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="cursor-pointer rounded-md bg-rose-300 p-4 text-lg font-bold transition-colors duration-200 ease-in-out hover:bg-rose-200"
+                        >
+                            Login
+                        </button>
+                    </form>
+                </section>
 
-                    <button type="submit">Login</button>
-                </form>
-            </section>
+                <section>
+                    <p className="text-center">
+                        Don't have an account? <a href="" className="text-rose-400 font-bold hover:text-rose-300">Sign Up</a>
+                    </p>
+                </section>
+            </div>
         </UserLayout>
     );
 }
