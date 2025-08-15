b9e926a1e5760f59bbd2ee0adf050fff3fca1b16
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-15 09:12:13 +0800

    feat(Register): create Register page

---
+++ resources/js/User/Pages/Auth/Register.jsx
@@ -0,0 +1,11 @@
+import UserLayout from "@User/Layout/UserLayout";
+
+function Register() {
+    return ( 
+        <UserLayout>
+            <div className=""></div>
+        </UserLayout>
+     );
+}
+
+export default Register;
