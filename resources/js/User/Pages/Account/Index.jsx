9f338a125e9dc85592d006babfa27ee027dfc5f2
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-16 09:00:02 +0800

    style(Account): add styles to Account page

--- resources/js/User/Pages/Account/Index.jsx
+++ resources/js/User/Pages/Account/Index.jsx
@@ -1,34 +1,70 @@
 import UserLayout from "@User/Layout/UserLayout";
 import AccountVisual from "@Assets/visuals/pexels-soldiervip-1391498.jpg";
 import { Menu } from "lucide-react";
+import { useState } from "react";
 
-function Index() {
+function Index({ children }) {
+    const [dropdownMenuState, setDropdownMenuState] = useState(false);
+
     return (
         <UserLayout>
-            <section>
-                <figure>
-                    <img src={AccountVisual} alt="" />
+            <section className="mb-4 flex flex-col items-center gap-4 rounded-md bg-rose-100 py-6">
+                <figure className="aspect-square w-16 overflow-hidden rounded-full">
+                    <img
+                        src={AccountVisual}
+                        alt=""
+                        className="h-full w-full object-cover object-[0_20%]"
+                    />
                 </figure>
-                <h1>Maria Llewelyn</h1>
+                <h1 className="font-quattrocento text-2xl font-bold">
+                    Maria Llewelyn
+                </h1>
+            </section>
+
+            <section className="relative mb-4 flex justify-end">
+                <div className="">
+                    <button
+                        className="flex cursor-pointer items-center gap-2 rounded-md border border-gray-400 bg-gray-200 px-4 py-2 active:bg-gray-300"
+                        onClick={() => setDropdownMenuState(!dropdownMenuState)}
+                    >
+                        <Menu size={24} strokeWidth={1.5} />
+                        <p>Menu</p>
+                    </button>
+
+                    <div
+                        className={`absolute left-0 mt-4 w-full rounded-md border border-gray-300 bg-gray-200 px-4 ${dropdownMenuState ? "visible opacity-100" : "invisible opacity-0"} transition duration-200 ease-in-out`}
+                    >
+                        <ul className="divide-y divide-gray-400">
+                            <li className="py-3">
+                                <button>Details</button>
+                            </li>
+                            <li className="py-3">
+                                <button>Address Book</button>
+                            </li>
+                            <li className="py-3">
+                                <button>Orders</button>
+                            </li>
+                            <li className="py-3">
+                                <button>Account</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </section>
 
             <div className="">
-                <button>
-                    <Menu size={24} strokeWidth={1.5} />
-                    <p>Menu</p>
-                </button>
-                <aside>
-                    <ul>
-                        <li>
+                <aside className="hidden">
+                    <ul className="">
+                        <li className="">
                             <button>Details</button>
                         </li>
-                        <li>
+                        <li className="">
                             <button>Address Book</button>
                         </li>
-                        <li>
+                        <li className="">
                             <button>Orders</button>
                         </li>
-                        <li>
+                        <li className="">
                             <button>Account</button>
                         </li>
                     </ul>
