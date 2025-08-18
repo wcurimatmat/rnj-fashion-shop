e468e493ff78b2d6aeb091a86cb3b5bd27f7cf07
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-18 08:48:45 +0800

    style(AddressBook): add styles for AddressBook component

--- resources/js/User/Pages/Account/AddressBook.jsx
+++ resources/js/User/Pages/Account/AddressBook.jsx
@@ -1,5 +1,5 @@
 import Index from "./Index";
-import { Check, Delete } from "lucide-react";
+import { MapPinCheck, MapPinMinus, MapPinPlus, SquarePen } from "lucide-react";
 
 function AddressBook() {
     return (
@@ -10,38 +10,121 @@ function AddressBook() {
                 </header>
             </section>
 
-            <section>
-                <ul>
-                    <li>
-                        <div className="">
-                            <div className="">
-                                <p>
-                                    566 Shaw Boulevard, Mandaluyong City, Metro
-                                    Manila, Philippines
-                                </p>
+            <div className="max-w-lg">
+                <section className="mb-8">
+                    <ul className="grid gap-4">
+                        <li className="rounded-md border-2 border-rose-300 bg-rose-100 px-4 py-6">
+                            <div className="grid gap-6">
                                 <div className="">
-                                    <p>Current Address</p>
+                                    <p className="leading-5 font-bold lg:w-[32ch]">
+                                        566 Shaw Boulevard, Mandaluyong City,
+                                        Metro Manila, Philippines
+                                    </p>
+                                </div>
+                                <div className="flex items-center justify-between">
+                                    <div className="text-sm font-bold text-rose-500 uppercase">
+                                        <p>Current Address</p>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <form className="flex gap-4">
+                                            <button
+                                                className="cursor-pointer"
+                                                type="submit"
+                                            >
+                                                <MapPinMinus
+                                                    size={20}
+                                                    strokeWidth={2}
+                                                />
+                                            </button>
+                                            <button
+                                                className="cursor-pointer"
+                                                type="submit"
+                                                disabled
+                                            >
+                                                <MapPinCheck
+                                                    size={20}
+                                                    strokeWidth={2}
+                                                    className="text-rose-500"
+                                                />
+                                            </button>
+                                            <button
+                                                className="cursor-pointer"
+                                                type="submit"
+                                            ></button>
+                                        </form>
+                                        <a href="">
+                                            {" "}
+                                            <SquarePen
+                                                size={20}
+                                                strokeWidth={2}
+                                                className=""
+                                            />
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
-
-                            <div className="">
-                                {/* Actions here: Set as current address | Delete Address */}
-
-                                <button>
-                                    <Delete size={20} strokeWidth={1.5} />
-                                </button>
-                                <button>
-                                    <Check size={20} strokeWidth={1.5} />
-                                </button>
+                        </li>
+                        <li className="rounded-md border-2 border-gray-300 bg-gray-100 px-4 py-6">
+                            <div className="grid gap-6">
+                                <div className="">
+                                    <p className="leading-5 font-bold lg:w-[32ch]">
+                                        566 Shaw Boulevard, Mandaluyong City,
+                                        Metro Manila, Philippines
+                                    </p>
+                                </div>
+                                <div className="flex items-center justify-between">
+                                    <div className="text-sm font-bold text-rose-500 uppercase">
+                                        <p className="hidden">
+                                            Current Address
+                                        </p>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <form className="flex gap-4">
+                                            <button
+                                                className="cursor-pointer"
+                                                type="submit"
+                                            >
+                                                <MapPinMinus
+                                                    size={20}
+                                                    strokeWidth={2}
+                                                />
+                                            </button>
+                                            <button
+                                                className="cursor-pointer"
+                                                type="submit"
+                                                disabled
+                                            >
+                                                <MapPinCheck
+                                                    size={20}
+                                                    strokeWidth={2}
+                                                />
+                                            </button>
+                                            <button
+                                                className="cursor-pointer"
+                                                type="submit"
+                                            ></button>
+                                        </form>
+                                        <a href="">
+                                            {" "}
+                                            <SquarePen
+                                                size={20}
+                                                strokeWidth={2}
+                                                className=""
+                                            />
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </li>
-                </ul>
-            </section>
-
-            <section>
-                <button>Add Address</button>
-            </section>
+                        </li>
+                    </ul>
+                </section>
+                <section>
+                    <button className="flex w-full cursor-pointer items-center justify-center gap-2 rounded-md bg-rose-300 py-5 font-bold transition-colors duration-200 ease-in-out hover:bg-rose-200 active:bg-rose-200">
+                        <MapPinPlus size={20} />
+                        <p>Add Address</p>
+                    </button>
+                </section>
+            </div>
         </>
     );
 }
