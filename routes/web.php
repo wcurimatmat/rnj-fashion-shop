781a3aaf0044e08d1b64c17918e2b18a31ca7be4
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-22 11:07:15 +0800

    refactor(routes/users): change component name to render

--- routes/web.php
+++ routes/web.php
@@ -4,45 +4,45 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return Inertia::render('Home/Index');
+    return Inertia::render('User/Home/Index');
 });
 
 Route::get('/shop', function () {
-    return Inertia::render("Shop/Index");
+    return Inertia::render("User/Shop/Index");
 });
 
 Route::get('/about', function () {
-    return Inertia::render("About/Index");
+    return Inertia::render("User/About/Index");
 });
 
 Route::get('/shop/product-view', function () {
-    return Inertia::render("ProductView/Index");
+    return Inertia::render("User/ProductView/Index");
 });
 
 Route::get("/account/login", function () {
-    return Inertia::render('Auth/Login');
+    return Inertia::render('User/Auth/Login');
 })->name('login');
 
 Route::get('/account/register', function () {
-    return Inertia::render('Auth/Register');
+    return Inertia::render('User/Auth/Register');
 })->name('register');
 
 Route::get('/cart', function () {
-    return Inertia::render("Cart/Index");
+    return Inertia::render("User/Cart/Index");
 });
 
 Route::get('/account', function () {
-    return Inertia::render('Account/Orders/Index');
+    return Inertia::render('User/Account/Orders/Index');
 })->name('account.orders.index');
 
 Route::get('/account/details', function () {
-    return Inertia::render('Account/Details/Index');
+    return Inertia::render('User/Account/Details/Index');
 })->name('account.details.index');
 
 Route::get('/account/address-book', function () {
-    return Inertia::render('Account/AddressBook/Index');
+    return Inertia::render('User/Account/AddressBook/Index');
 })->name('account.address-book.index');
 
 Route::get('/account/address-book/create', function () {
-    return Inertia::render('Account/AddressBook/Create');
+    return Inertia::render('User/Account/AddressBook/Create');
 })->name('account.address-book.create');
