59ced7886d6aa36fa499fedf3ce585592663360b
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-22 10:24:16 +0800

    feat(bootstrap): refactor routes file registration and register the route file for admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,9 +7,16 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        using: function () {
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+
+            Route::middleware('web')
+                ->prefix('admin')
+                ->group(base_path('routes/admin.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(append: [
