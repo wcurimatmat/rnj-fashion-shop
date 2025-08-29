5948b26ea9df1c35e282da0ef7189776e1580915
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-29 18:26:18 +0800

    feat: use account menu component

--- resources/js/Layouts/Users/AccountLayout.jsx
+++ resources/js/Layouts/Users/AccountLayout.jsx
@@ -1,13 +1,10 @@
 import UserLayout from "@layouts/Users/UserLayout";
 import AccountVisual from "@assets/visuals/pexels-soldiervip-1391498.jpg";
-import { Menu } from "lucide-react";
 import { Link, usePage } from "@inertiajs/react";
-import { useState } from "react";
+import AccountMenu from "@/Components/Users/AccountMenu/AccountMenu";
 
 function AccountLayout({ children }) {
     const { component } = usePage();
-
-    const [dropdownMenuState, setDropdownMenuState] = useState(false);
 
     return (
         <UserLayout>
@@ -25,52 +22,7 @@ function AccountLayout({ children }) {
             </section>
 
             <section className="relative mb-4 flex justify-end min-[1200px]:hidden">
-                <div className="">
-                    <button
-                        className="flex cursor-pointer items-center gap-2 rounded-md border border-gray-400 bg-gray-200 px-4 py-2 active:bg-gray-300"
-                        onClick={() => setDropdownMenuState(!dropdownMenuState)}
-                    >
-                        <Menu size={24} strokeWidth={1.5} />
-                        <p>Menu</p>
-                    </button>
-
-                    <div
-                        className={`absolute left-0 mt-4 w-full rounded-md border border-gray-300 bg-gray-200 px-4 md:px-8 ${dropdownMenuState ? "visible opacity-100" : "invisible opacity-0"} transition duration-200 ease-in-out`}
-                    >
-                        <ul className="text-md divide-y divide-gray-400">
-                            <li>
-                                <Link href={route("account.orders.index")}>
-                                    <p className="w-full py-3 text-left md:py-5">
-                                        Orders
-                                    </p>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href={route("account.details.index")}>
-                                    <p className="w-full py-3 text-left md:py-5">
-                                        Details
-                                    </p>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    href={route("account.address-book.index")}
-                                >
-                                    <p className="w-full py-3 text-left md:py-5">
-                                        Address Book
-                                    </p>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="">
-                                    <p className="w-full py-3 text-left md:py-5">
-                                        Account
-                                    </p>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                <AccountMenu />
             </section>
 
             <div className="min-[1200px]:mt-20 min-[1200px]:flex min-[1200px]:gap-20 min-[1350px]:gap-40">
