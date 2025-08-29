500f3782ce0591647aff2217752d47b83736bad1
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-29 17:58:26 +0800

    feat: use the button component from shadcn

--- resources/js/Pages/User/Home/Promotional.jsx
+++ resources/js/Pages/User/Home/Promotional.jsx
@@ -1,3 +1,5 @@
+import { Button } from "@/Shadcn/components/ui/button";
+
 function Promotional() {
     return (
         <section className="promotional-background rounded-lg py-36 font-bold text-gray-100 lg:py-56">
@@ -5,12 +7,13 @@ function Promotional() {
                 <h2 className="font-quattrocento m-auto text-2xl md:max-w-sm md:text-4xl md:max-lg:leading-14 lg:max-w-xl lg:text-5xl lg:leading-14">
                     Chic, Comfortable, and Always on Trend
                 </h2>
-                <a
-                    href=""
-                    className="m-auto w-max rounded-sm bg-rose-300 px-8 py-4"
+
+                <Button
+                    asChild
+                    className="text-md m-auto w-max bg-rose-300 p-6 hover:bg-rose-400 md:text-lg"
                 >
-                    <p>Explore Collections</p>
-                </a>
+                    <a href="#">Explore Collection</a>
+                </Button>
             </div>
         </section>
     );
