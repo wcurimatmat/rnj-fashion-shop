d12e38bda494195028ed8e62741328f275012a4e
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-30 10:08:28 +0800

    feat: add route for dashboard page

--- routes/admin.php
+++ routes/admin.php
@@ -8,3 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/login', function () {
     return Inertia::render('Admin/Auth/Login');
 });
+
+Route::get("/dashboard", function () {
+    return Inertia::render('Admin/Dashboard/Index');
+});
