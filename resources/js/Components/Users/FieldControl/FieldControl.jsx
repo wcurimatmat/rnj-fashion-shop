e8b9ad60b09747ca2b4ecf9f2a3f13b5f5c8463e
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-30 09:12:26 +0800

    refactor: remove Input component to make the FieldControl component a wrapper of any input types

--- resources/js/Components/Users/FieldControl/FieldControl.jsx
+++ resources/js/Components/Users/FieldControl/FieldControl.jsx
@@ -3,7 +3,7 @@ import { Label } from "@/Shadcn/components/ui/label";
 function FieldControl({ id, label, children }) {
     return (
         <div className="grid gap-2">
-            <Label htmlFor={id}>{label}</Label>
+            <Label htmlFor={id} className="font-bold text-gray-600" >{label}</Label>
             {children}
         </div>
     );
