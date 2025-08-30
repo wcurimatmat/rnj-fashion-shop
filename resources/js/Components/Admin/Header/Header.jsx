fec094480132c38a6d4c1b0d116d64d02dec41b3
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-30 11:24:35 +0800

    style: adjust header padding

--- resources/js/Components/Admin/Header/Header.jsx
+++ resources/js/Components/Admin/Header/Header.jsx
@@ -1,14 +1,13 @@
 import { Menu } from "lucide-react";
 import { Button } from "@/Shadcn/components/ui/button";
 import Dropdown from "@/Components/Admin/Dropdown/Dropdown";
+import SidebarDrawer from "../SidebarDrawer/SidebarDrawer";
 
 function Header() {
     return (
         <header>
-            <div className="flex items-center justify-between p-4">
-                <Button size="icon" variant="ghost">
-                    <Menu />
-                </Button>
+            <div className="flex items-center justify-between py-3">
+                <SidebarDrawer />
                 <Dropdown />
             </div>
         </header>
