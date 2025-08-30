48676dd25697ca29ac84b168a0722dd51bdebf95
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-30 09:54:35 +0800

    style: add width constraints

--- resources/js/Layouts/Admin/AuthLayout.jsx
+++ resources/js/Layouts/Admin/AuthLayout.jsx
@@ -1,7 +1,7 @@
 function AuthLayout({ children }) {
     return (
         <main className="flex h-screen items-center justify-center">
-            <div className="grid flex-1 gap-10 px-4">{children}</div>
+            <div className="grid flex-1 gap-10 px-4 md:max-w-md">{children}</div>
         </main>
     );
 }
