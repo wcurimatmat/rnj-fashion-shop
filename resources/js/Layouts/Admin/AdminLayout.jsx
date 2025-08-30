f8d387cb0395d3e3a9707ecafe454ada7aead87f
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-30 13:26:00 +0800

    style: adjust padding-block

--- resources/js/Layouts/Admin/AdminLayout.jsx
+++ resources/js/Layouts/Admin/AdminLayout.jsx
@@ -6,7 +6,7 @@ function AdminLayout({ children }) {
         <div className="md:flex md:justify-between md:items-start">
             <Sidebar />
             <main className="flex-1">
-                <div className="grid gap-8 p-4 md:px-6">
+                <div className="grid gap-8 p-4 md:p-6">
                     <Header />
                     {children}
                 </div>
