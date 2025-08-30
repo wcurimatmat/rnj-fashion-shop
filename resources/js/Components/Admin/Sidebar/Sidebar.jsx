2916fcef092e9e204da5b8dd1846e3e1ca4875c2
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-30 13:25:30 +0800

    style: add box shadow and the Navigation component

--- resources/js/Components/Admin/Sidebar/Sidebar.jsx
+++ resources/js/Components/Admin/Sidebar/Sidebar.jsx
@@ -1,14 +1,21 @@
 import ShopLogo from "@/Assets/logo/logo.png";
+import Navigation from "../Navigation/Navigation";
 
 function Sidebar() {
     return (
-        <aside className="sticky top-0 hidden h-screen border-0 border-r border-r-gray-400/50 px-6 py-3 md:block md:basis-[280px]">
-            <section className="flex items-center gap-2">
-                <figure className="aspect-square w-10">
-                    <img src={ShopLogo} alt="object-cover w-full h-full" />
-                </figure>
-                <p className="text-base font-bold">R&J Fashion Shop</p>
-            </section>
+        <aside className="sticky top-0 hidden h-screen p-6 shadow-[0_3px_10px_rgb(0,0,0,0.2)] md:block md:basis-[280px]">
+            <div className="grid gap-8">
+                <section className="flex items-center gap-2">
+                    <figure className="aspect-square w-10">
+                        <img src={ShopLogo} alt="object-cover w-full h-full" />
+                    </figure>
+                    <p className="text-base font-bold">R&J Fashion Shop</p>
+                </section>
+
+                <section>
+                    <Navigation />
+                </section>
+            </div>
         </aside>
     );
 }
