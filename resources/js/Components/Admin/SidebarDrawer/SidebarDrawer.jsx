08ad725926b75735742cd802eb7f3f7f45f94e85
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-30 11:38:38 +0800

    feat: make the drawer render conditionally

--- resources/js/Components/Admin/SidebarDrawer/SidebarDrawer.jsx
+++ resources/js/Components/Admin/SidebarDrawer/SidebarDrawer.jsx
@@ -11,45 +11,46 @@ import ShopLogo from "@/Assets/logo/logo.png";
 
 function SidebarDrawer() {
     return (
-        <Drawer direction="left">
-            <DrawerTrigger>
-                <Menu />
-            </DrawerTrigger>
-
-            <DrawerContent className="p-4">
-                <DrawerClose className="flex justify-end">
-                    <Button variant="ghost" size="icon" className="">
-                        <X className="size-5" />
-                    </Button>
-                </DrawerClose>
-
-                <div className="grid gap-10">
-                    <section className="flex items-center gap-2">
-                        <figure className="aspect-square w-12">
-                            <img
-                                src={ShopLogo}
-                                alt="object-cover w-full h-full"
-                            />
-                        </figure>
-                        <p className="text-lg font-bold">R&J Fashion Shop</p>
-                    </section>
-
-                    <nav>
-                        <ul>
-                            <li>
-                                <Link
-                                    href="#"
-                                    className="flex items-center gap-2"
-                                >
-                                    <LayoutDashboard strokeWidth={1.5} />
-                                    <span>Dashboard</span>
-                                </Link>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </DrawerContent>
-        </Drawer>
+        <div className="inline-flex md:hidden">
+            <Drawer direction="left">
+                <DrawerTrigger>
+                    <Menu />
+                </DrawerTrigger>
+                <DrawerContent className="p-4">
+                    <DrawerClose className="flex justify-end">
+                        <Button variant="ghost" size="icon" className="">
+                            <X className="size-5" />
+                        </Button>
+                    </DrawerClose>
+                    <div className="grid gap-10">
+                        <section className="flex items-center gap-2">
+                            <figure className="aspect-square w-12">
+                                <img
+                                    src={ShopLogo}
+                                    alt="object-cover w-full h-full"
+                                />
+                            </figure>
+                            <p className="text-lg font-bold">
+                                R&J Fashion Shop
+                            </p>
+                        </section>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <Link
+                                        href="#"
+                                        className="flex items-center gap-2"
+                                    >
+                                        <LayoutDashboard strokeWidth={1.5} />
+                                        <span>Dashboard</span>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </DrawerContent>
+            </Drawer>
+        </div>
     );
 }
 
