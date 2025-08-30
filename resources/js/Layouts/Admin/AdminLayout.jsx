8502b1cfa2e9e73dd39f41d79d73a84958bcfd0d
wcurimatmat <wcurimatmat24@gmail.com>
2025-08-30 12:09:37 +0800

    refactor: refactor the markup for AdminLayout

--- resources/js/Layouts/Admin/AdminLayout.jsx
+++ resources/js/Layouts/Admin/AdminLayout.jsx
@@ -1,13 +1,17 @@
 import Header from "@/Components/Admin/Header/Header";
+import Sidebar from "@/Components/Admin/Sidebar/Sidebar";
 
 function AdminLayout({ children }) {
     return (
-        <>
-            <div className="grid gap-8 p-4 md:px-16 lg:px-24">
-                <Header />
-                <main>{children}</main>
-            </div>
-        </>
+        <div className="md:flex md:justify-between md:items-start">
+            <Sidebar />
+            <main className="flex-1">
+                <div className="grid gap-8 p-4 md:px-6">
+                    <Header />
+                    {children}
+                </div>
+            </main>
+        </div>
     );
 }
 
