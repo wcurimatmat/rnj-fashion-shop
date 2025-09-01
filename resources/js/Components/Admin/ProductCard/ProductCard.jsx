c2418dbbf524e174860ecd549cbfadc408ac751a
wcurimatmat <wcurimatmat24@gmail.com>
2025-09-01 14:12:21 +0800

    feat: add ProductCard component

--- resources/js/Components/Admin/ProductCard/ProductCard.jsx
+++ resources/js/Components/Admin/ProductCard/ProductCard.jsx
@@ -8,11 +8,12 @@ import {
     CardHeader,
     CardTitle,
 } from "@/Shadcn/components/ui/card";
+import { Link } from "@inertiajs/react";
 
 function ProducCard() {
     return (
-        <Card className="gap-3 py-4 rounded-md">
-            <CardContent className="px-4">
+        <Card className="gap-3 rounded-md py-5">
+            <CardContent className="px-5">
                 <figure className="aspect-[3/4]">
                     <img
                         src={ProductCardVisual}
@@ -22,13 +23,18 @@ function ProducCard() {
                 </figure>
             </CardContent>
 
-            <CardHeader className="px-4">
+            <CardHeader className="px-5">
                 <CardTitle className="text-lg">Item 6</CardTitle>
                 <CardDescription>$1,250.00</CardDescription>
             </CardHeader>
 
-            <CardFooter className="px-4">
-                <Button className="w-full">Edit Product</Button>
+            <CardFooter className="px-5">
+                <Button
+                    className="primary-button rounded w-full p-6 text-base font-bold text-gray-800"
+                    asChild
+                >
+                    <Link href="#">Edit Product</Link>
+                </Button>
             </CardFooter>
         </Card>
     );
