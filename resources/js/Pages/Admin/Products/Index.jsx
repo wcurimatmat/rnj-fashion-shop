680c6adf3b3d1b9e8f66d202022ba6af4f9a8160
wcurimatmat <wcurimatmat24@gmail.com>
2025-09-01 13:50:58 +0800

    feat: add ProductCard component

---
+++ resources/js/Pages/Admin/Products/Index.jsx
@@ -0,0 +1,68 @@
+import { Plus } from "lucide-react";
+
+import { Link } from "@inertiajs/react";
+import AdminLayout from "@/Layouts/Admin/AdminLayout";
+import { Button } from "@/Shadcn/components/ui/button";
+import ProductCard from "@/Components/Admin/ProductCard/ProductCard";
+
+function Index() {
+    return (
+        <AdminLayout>
+            <div className="space-y-8">
+                <section>
+                    <h1 className="text-2xl">Products</h1>
+                </section>
+
+                <section>
+                    <div className="flex items-center justify-between">
+                        <Button className="p-6 text-base" variant="secondary">
+                            Filter
+                        </Button>
+                        <Button
+                            className="primary-button p-6 text-base"
+                            asChild
+                        >
+                            <Link>
+                                <Plus className="size-5" />
+                                <span>Add Product</span>
+                            </Link>
+                        </Button>
+                    </div>
+                </section>
+
+                <section>
+                    <ul className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
+                        <li>
+                            <ProductCard />
+                        </li>
+                        <li>
+                            <ProductCard />
+                        </li>
+                        <li>
+                            <ProductCard />
+                        </li>
+                        <li>
+                            <ProductCard />
+                        </li>
+                        <li>
+                            <ProductCard />
+                        </li>
+                        <li>
+                            <ProductCard />
+                        </li>
+                        <li>
+                            <ProductCard />
+                        </li>
+                        <li>
+                            <ProductCard />
+                        </li>
+                    </ul>
+
+                    {/* Paginations */}
+                </section>
+            </div>
+        </AdminLayout>
+    );
+}
+
+export default Index;
