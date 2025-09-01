400f270597137cf5bed4f9572ef39ed46c80343c
wcurimatmat <wcurimatmat24@gmail.com>
2025-09-01 14:12:10 +0800

    feat: add Paginate component

---
+++ resources/js/Components/Shared/Paginate/Paginate.jsx
@@ -0,0 +1,32 @@
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+} from "@/Shadcn/components/ui/pagination";
+
+function Paginate() {
+    return (
+        <Pagination>
+            <PaginationContent>
+                <PaginationItem>
+                    <PaginationPrevious href="#" />
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationLink href="#">1</PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationEllipsis />
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationNext href="#" />
+                </PaginationItem>
+            </PaginationContent>
+        </Pagination>
+    );
+}
+
+export default Paginate;
