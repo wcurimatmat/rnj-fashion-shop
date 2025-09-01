f636eb9487e281b14434e9055d4655cf73726f9e
wcurimatmat <wcurimatmat24@gmail.com>
2025-09-01 10:50:28 +0800

    feat: add scroll-area component from shadcn

---
+++ resources/js/Components/Admin/OrderTable/OrderTable.jsx
@@ -0,0 +1,153 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/Shadcn/components/ui/card";
+
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/Shadcn/components/ui/table";
+
+import { ScrollArea } from "@/Shadcn/components/ui/scroll-area";
+
+function OrderTable() {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Orders</CardTitle>
+                <CardDescription>Lorem ipsum dolor sit amet.</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <ScrollArea className="h-80 pr-4">
+                    <Table className="">
+                        <TableHeader className="sticky top-0 bg-white">
+                            <TableRow>
+                                <TableHead className="w-[100px] font-bold">
+                                    Invoice
+                                </TableHead>
+                                <TableHead className="font-bold">
+                                    Status
+                                </TableHead>
+                                <TableHead className="font-bold">
+                                    Method
+                                </TableHead>
+                                <TableHead className="text-right font-bold">
+                                    Amount
+                                </TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody className="w-full">
+                            {invoices.map((invoice) => (
+                                <TableRow key={invoice.invoice}>
+                                    <TableCell className="font-medium">
+                                        {invoice.invoice}
+                                    </TableCell>
+                                    <TableCell>
+                                        {invoice.paymentStatus}
+                                    </TableCell>
+                                    <TableCell>
+                                        {invoice.paymentMethod}
+                                    </TableCell>
+                                    <TableCell className="text-right">
+                                        {invoice.totalAmount}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </ScrollArea>
+            </CardContent>
+        </Card>
+    );
+}
+
+const invoices = [
+    {
+        invoice: "INV001",
+        paymentStatus: "Paid",
+        totalAmount: "$250.00",
+        paymentMethod: "Credit Card",
+    },
+    {
+        invoice: "INV002",
+        paymentStatus: "Pending",
+        totalAmount: "$150.00",
+        paymentMethod: "PayPal",
+    },
+    {
+        invoice: "INV003",
+        paymentStatus: "Unpaid",
+        totalAmount: "$350.00",
+        paymentMethod: "Bank Transfer",
+    },
+    {
+        invoice: "INV004",
+        paymentStatus: "Paid",
+        totalAmount: "$450.00",
+        paymentMethod: "Credit Card",
+    },
+    {
+        invoice: "INV005",
+        paymentStatus: "Paid",
+        totalAmount: "$550.00",
+        paymentMethod: "PayPal",
+    },
+    {
+        invoice: "INV006",
+        paymentStatus: "Pending",
+        totalAmount: "$200.00",
+        paymentMethod: "Bank Transfer",
+    },
+    {
+        invoice: "INV007",
+        paymentStatus: "Unpaid",
+        totalAmount: "$300.00",
+        paymentMethod: "Credit Card",
+    },
+    {
+        invoice: "INV007",
+        paymentStatus: "Unpaid",
+        totalAmount: "$300.00",
+        paymentMethod: "Credit Card",
+    },
+    {
+        invoice: "INV007",
+        paymentStatus: "Unpaid",
+        totalAmount: "$300.00",
+        paymentMethod: "Credit Card",
+    },
+    {
+        invoice: "INV007",
+        paymentStatus: "Unpaid",
+        totalAmount: "$300.00",
+        paymentMethod: "Credit Card",
+    },
+    {
+        invoice: "INV007",
+        paymentStatus: "Unpaid",
+        totalAmount: "$300.00",
+        paymentMethod: "Credit Card",
+    },
+    {
+        invoice: "INV007",
+        paymentStatus: "Unpaid",
+        totalAmount: "$300.00",
+        paymentMethod: "Credit Card",
+    },
+    {
+        invoice: "INV007",
+        paymentStatus: "Unpaid",
+        totalAmount: "$300.00",
+        paymentMethod: "Credit Card",
+    },
+];
+
+export default OrderTable;
