4987007e5c036fec5aefbb6d19c24f7f1e154078
wcurimatmat <wcurimatmat24@gmail.com>
2025-09-02 16:32:15 +0800

    feat: add TableRowAction component

--- resources/js/Pages/Admin/Orders/Columns.jsx
+++ resources/js/Pages/Admin/Orders/Columns.jsx
@@ -1,14 +1,8 @@
-import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import { ArrowUpDown } from "lucide-react";
 
 import { Button } from "@/Shadcn/components/ui/button";
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@/Shadcn/components/ui/dropdown-menu";
+
+import TableRowAction from "@/Components/Shared/TableRowAction/TableRowAction";
 
 export const columns = [
     { accessorKey: "id", header: () => "id" },
@@ -51,29 +45,7 @@ export const columns = [
 
             return (
                 <div className="flex justify-end">
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="ghost" className="h-8 w-8 p-0">
-                                <span className="sr-only">Open menu</span>
-                                <MoreHorizontal className="h-4 w-4" />
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end">
-                            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                            <DropdownMenuItem
-                                onClick={() =>
-                                    navigator.clipboard.writeText(payment.id)
-                                }
-                            >
-                                Copy payment ID
-                            </DropdownMenuItem>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuItem>View customer</DropdownMenuItem>
-                            <DropdownMenuItem>
-                                View payment details
-                            </DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
+                    <TableRowAction row={payment} />
                 </div>
             );
         },
