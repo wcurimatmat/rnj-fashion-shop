89ef4655e73ecda4d8af57a06d24efd8c9fe716a
wcurimatmat <wcurimatmat24@gmail.com>
2025-09-02 12:03:21 +0800

    feat: create PageHeader component

---
+++ resources/js/Components/Admin/PageHeader/PageHeader.jsx
@@ -0,0 +1,9 @@
+function PageHeader({ title }) {
+    return (
+        <section>
+            <h1 className="font-quattrocento text-2xl font-bold">{title}</h1>
+        </section>
+    );
+}
+
+export default PageHeader;
