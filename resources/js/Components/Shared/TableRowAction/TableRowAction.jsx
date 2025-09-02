838e3dd971aaa18b0d6032416b7975115a21b9d3
wcurimatmat <wcurimatmat24@gmail.com>
2025-09-02 16:32:03 +0800

    feat: create TableRowAction component

---
+++ resources/js/Components/Shared/TableRowAction/TableRowAction.jsx
@@ -0,0 +1,37 @@
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/Shadcn/components/ui/dropdown-menu";
+import { Button } from "@/Shadcn/components/ui/button";
+
+import { MoreHorizontal } from "lucide-react";
+
+function TableRowAction({ row }) {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                    <span className="sr-only">Open menu</span>
+                    <MoreHorizontal className="h-4 w-4" />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                <DropdownMenuItem
+                    onClick={() => navigator.clipboard.writeText(row.id)}
+                >
+                    Copy payment ID
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>View customer</DropdownMenuItem>
+                <DropdownMenuItem>View payment details</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
+
+export default TableRowAction;
