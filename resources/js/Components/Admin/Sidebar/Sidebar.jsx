e42fe5aae97edc08a968df01a221b56cff06785d
wcurimatmat <wcurimatmat24@gmail.com>
2025-10-19 13:19:18 +0800

    make the sidebar scrollable

--- resources/js/Components/Admin/Sidebar/Sidebar.jsx
+++ resources/js/Components/Admin/Sidebar/Sidebar.jsx
@@ -3,7 +3,7 @@ import Navigation from "../Navigation/Navigation";
 
 function Sidebar() {
     return (
-        <aside className="sticky top-0 hidden h-screen p-6 shadow-[0_3px_10px_rgb(0,0,0,0.2)] md:block md:basis-[280px]">
+        <aside className="sticky top-0 hidden h-screen overflow-scroll p-6 shadow-[0_3px_10px_rgb(0,0,0,0.2)] md:block md:basis-[280px]">
             <div className="grid gap-8">
                 <section className="flex items-center gap-2">
                     <figure className="aspect-square w-10">
