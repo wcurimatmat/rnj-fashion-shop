46d9aa6ce163695222d759f291e9dbd03a707975
wcurimatmat <wcurimatmat24@gmail.com>
2025-10-19 12:59:02 +0800

    refactor navigation component

--- resources/js/Components/Admin/Navigation/Navigation.jsx
+++ resources/js/Components/Admin/Navigation/Navigation.jsx
@@ -1,14 +1,44 @@
-import ContentManagement from "./ContentManagement";
-import Settings from "./Settings";
+import {
+    Box,
+    ChartNoAxesCombined,
+    LayoutDashboard,
+    Megaphone,
+    ShoppingBag,
+    UsersRound,
+} from "lucide-react";
 
-import NavigationItem from "./NavigationItem";
+import Settings from "./Settings";
+import NavigationLink from "./NavigationLink";
+import ContentManagement from "./ContentManagement";
+
+const navigationRoutes = {
+    dashboard: route("dashboard.index"),
+    products: route("products.index"),
+    orders: route("orders.index"),
+    reportsAndAnalytics: "#",
+    promotionsAndMarketing: "#",
+};
 
 function Navigation() {
     return (
         <nav>
             <ul>
-                <NavigationItem />
-
+                <li className="rounded-md hover:bg-gray-200">
+                    <NavigationLink
+                        icon={LayoutDashboard}
+                        name="Dashboard"
+                        url={navigationRoutes.dashboard}
+                    />
+                </li>
+                <li className="rounded-md hover:bg-gray-200">
+                    Products Management
+                </li>
+                <li className="rounded-md hover:bg-gray-200">
+                    Orders Management
+                </li>
+                <li className="rounded-md hover:bg-gray-200">
+                    Customers Management
+                </li>
                 <li>
                     <ContentManagement />
                 </li>
