6efd3c84fe3fdb9d2f7c72fd3805832b4575997a
wcurimatmat <wcurimatmat24@gmail.com>
2025-10-19 11:58:30 +0800

    refactor the JSX file by extracting data fof navigation link to create separate component

--- resources/js/Components/Admin/Navigation/Navigation.jsx
+++ resources/js/Components/Admin/Navigation/Navigation.jsx
@@ -1,59 +1,19 @@
-import { Link } from "@inertiajs/react";
-import {
-    Box,
-    ShoppingBag,
-    LayoutDashboard,
-    UsersRound,
-} from "lucide-react";
+import ContentManagement from "./ContentManagement";
+import Settings from "./Settings";
 
-import CollapsibleItem from "./CollapsibleItem";
-
-const links = [
-    {
-        name: "Dashboard",
-        url: route("dashboard.index"),
-        icon: <LayoutDashboard strokeWidth={1.4} />,
-    },
-    {
-        name: "Products",
-        url: route("products.index"),
-        icon: <Box strokeWidth={1.4} />,
-    },
-    {
-        name: "Orders",
-        url: route("orders.index"),
-        icon: <ShoppingBag strokeWidth={1.4} />,
-    },
-    {
-        name: "Customers",
-        url: route("customers.index"),
-        icon: <UsersRound strokeWidth={1.4} />,
-    },
-];
+import NavigationItem from "./NavigationItem";
 
 function Navigation() {
     return (
         <nav>
             <ul>
-                {links.map(function (link, index) {
-                    return (
-                        <li
-                            key={index}
-                            className="rounded-md hover:bg-gray-200"
-                        >
-                            <Link
-                                href={link.url}
-                                className="flex items-center gap-2 p-3"
-                            >
-                                {link.icon}
-                                <span className="text-base">{link.name}</span>
-                            </Link>
-                        </li>
-                    );
-                })}
+                <NavigationItem />
 
                 <li>
-                    <CollapsibleItem />
+                    <ContentManagement />
+                </li>
+                <li>
+                    <Settings />
                 </li>
             </ul>
         </nav>
