8d6203e2c46b2129296e2e4ef1115f03cf2670b2
wcurimatmat <wcurimatmat24@gmail.com>
2025-10-19 11:58:45 +0800

    create navigation-item component

---
+++ resources/js/Components/Admin/Navigation/NavigationItem.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "@inertiajs/react";
+import { Box, ShoppingBag, LayoutDashboard, UsersRound } from "lucide-react";
+
+const navigationItemLinks = [
+    {
+        name: "Dashboard",
+        url: route("dashboard.index"),
+        icon: <LayoutDashboard strokeWidth={1.4} />,
+    },
+    {
+        name: "Products",
+        url: route("products.index"),
+        icon: <Box strokeWidth={1.4} />,
+    },
+    {
+        name: "Orders",
+        url: route("orders.index"),
+        icon: <ShoppingBag strokeWidth={1.4} />,
+    },
+    {
+        name: "Customers",
+        url: route("customers.index"),
+        icon: <UsersRound strokeWidth={1.4} />,
+    },
+];
+
+const NavigationItem = () =>
+    navigationItemLinks.map(function (link, index) {
+        return (
+            <li key={index} className="rounded-md hover:bg-gray-200">
+                <Link href={link.url} className="flex items-center gap-2 p-3">
+                    {link.icon}
+                    <span className="text-base">{link.name}</span>
+                </Link>
+            </li>
+        );
+    });
+
+export default NavigationItem;
