72460fb089e9a54c403bf4c766676837409c3b22
wcurimatmat <wcurimatmat24@gmail.com>
2025-10-19 13:42:49 +0800

    update the sizing for shop logo and text

--- resources/js/Components/Admin/Sidebar/Sidebar.jsx
+++ resources/js/Components/Admin/Sidebar/Sidebar.jsx
@@ -6,10 +6,10 @@ function Sidebar() {
         <aside className="sticky top-0 hidden h-screen overflow-scroll p-6 shadow-[0_3px_10px_rgb(0,0,0,0.2)] md:block md:basis-[280px]">
             <div className="grid gap-8">
                 <section className="flex items-center gap-2">
-                    <figure className="aspect-square w-10">
+                    <figure className="aspect-square size-13">
                         <img src={ShopLogo} alt="object-cover w-full h-full" />
                     </figure>
-                    <p className="text-base font-bold">R&J Fashion Shop</p>
+                    <p className="text-xl font-bold">R&J Fashion Shop</p>
                 </section>
 
                 <section>
