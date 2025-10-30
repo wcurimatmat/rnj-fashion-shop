5baba207663d40b56a26a16698bcd242d1f83023
wcurimatmat <wcurimatmat24@gmail.com>
2025-10-30 12:28:53 +0800

    create Categories And Collections page

---
+++ resources/js/Pages/Admin/CategoriesAndCollections/Index.jsx
@@ -0,0 +1,14 @@
+import AdminLayout from "@/Layouts/Admin/AdminLayout";
+import PageHeader from "@/Components/Admin/PageHeader/PageHeader";
+
+const Index = () => {
+    return (
+        <AdminLayout>
+            <>
+                <PageHeader title="Categories & Collections" />
+            </>
+        </AdminLayout>
+    );
+};
+
+export default Index;
